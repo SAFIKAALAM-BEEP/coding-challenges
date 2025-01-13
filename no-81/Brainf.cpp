912fd82c3840df95606f0daa666fbf7465b5e962
Safika <sapikachu08@gmail.com>
2025-01-12 21:50:16 -0500

    takes too long to run Mandelbrot set. Need to optimize

--- no-81/Brainf.cpp
+++ no-81/Brainf.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <vector>
 
 class BrainfuckInterpreter {
@@ -45,8 +46,23 @@ public:
         code.clear();
     }
     
-    void interpret(const std::string& input) {
-        code = input;
+    void loadFromFile(const std::string& filename) {
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error: Could not open file " << filename << std::endl;
+            return;
+        }
+
+        code.clear();
+        std::string line;
+        while (std::getline(file, line)) {
+            code += line;
+        }
+        file.close();
+    }
+    
+    // For file
+    void interpret() {
         programCounter = 0;
         
         while (programCounter < code.length()) {
@@ -95,4 +111,56 @@ public:
             programCounter++;
         }
     }
+
+    // For user input 
+    // void interpret(const std::string& input) {
+    //     code = input;
+    //     programCounter = 0;
+        
+    //     while (programCounter < code.length()) {
+    //         char instruction = code[programCounter];
+            
+    //         switch (instruction) {
+    //             case '>':
+    //                 if (dataPointer < MEMORY_SIZE - 1) dataPointer++;
+    //                 break;
+                    
+    //             case '<':
+    //                 if (dataPointer > 0) dataPointer--;
+    //                 break;
+                    
+    //             case '+':
+    //                 memory[dataPointer]++;
+    //                 break;
+                    
+    //             case '-':
+    //                 memory[dataPointer]--;
+    //                 break;
+                    
+    //             case '.':
+    //                 std::cout << memory[dataPointer];
+    //                 break;
+                    
+    //             case ',':
+    //                 char input;
+    //                 std::cin >> std::noskipws >> input;
+    //                 memory[dataPointer] = input;
+    //                 break;
+                    
+    //             case '[':
+    //                 if (memory[dataPointer] == 0) {
+    //                     programCounter = findMatchingBracket(programCounter, true);
+    //                 }
+    //                 break;
+                    
+    //             case ']':
+    //                 if (memory[dataPointer] != 0) {
+    //                     programCounter = findMatchingBracket(programCounter, false);
+    //                 }
+    //                 break;
+    //         }
+            
+    //         programCounter++;
+    //     }
+    // }
 };
--- no-81/no-81.cpp
+++ no-81/no-81.cpp
@@ -3,25 +3,30 @@
 #include "Brainf.cpp"
 
 int main() {
-    BrainfuckInterpreter interpreter;
-    std::string input;
+    // BrainfuckInterpreter interpreter;
+    // std::string input;
     
-    std::cout << "Brainf*ck Interpreter (type 'exit' to quit, 'reset' to clear memory)\n";
-    std::cout << "Enter your code: ";
+    // std::cout << "Brainf*ck Interpreter (type 'exit' to quit, 'reset' to clear memory)\n";
+    // std::cout << "Enter your code: ";
     
-    while (true) {
-        std::getline(std::cin, input);
+    // while (true) {
+    //     std::getline(std::cin, input);
         
-        if (input == "exit") {
-            break;
-        } else if (input == "reset") {
-            interpreter.reset();
-            std::cout << "Memory reset.\n";
-        } else {
-            interpreter.interpret(input);
-            std::cout << "\nEnter your code: ";
-        }
-    }
+    //     if (input == "exit") {
+    //         break;
+    //     } else if (input == "reset") {
+    //         interpreter.reset();
+    //         std::cout << "Memory reset.\n";
+    //     } else {
+    //         interpreter.interpret(input);
+    //         std::cout << "\nEnter your code: ";
+    //     }
+    // }
+
+    BrainfuckInterpreter interpreter;
+    interpreter.loadFromFile("test.bf");
+    interpreter.interpret();
     
     return 0;
 }
+    
